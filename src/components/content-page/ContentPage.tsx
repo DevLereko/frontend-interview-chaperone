089482a2ede25b2d2c05f0f673e5ea2ed024d97a
Lereko Foloko <84933354+Boithuto2021@users.noreply.github.com>
2023-02-10 10:04:49 +0200

    added responsiveness on content page

--- src/components/content-page/ContentPage.tsx
+++ src/components/content-page/ContentPage.tsx
@@ -1,27 +1,34 @@
-import { Box, Heading, Stack, Text } from "@chakra-ui/react";
+import { Box, Heading, Flex, Text } from "@chakra-ui/react";
 import React from "react";
 
 export default function ContentPage() {
   return (
-    <Box>
-      <Stack
-        h="100vh"
-        pl="28.6em"
-        bg="#ff9200"
-        alignItems="center"
+    <Box
+      w="full"
+      py={{ base: "28.6px", md: "0px" }}
+      pl={{ base: "none", md: "28.6em" }}
+      bgColor="#ff9200"
+      minH="100vh"
+    >
+      <Flex
+        direction="column"
         justify="center"
-        spacing={10}
+        gap={8}
+        align="center"
+        minH="100vh"
+        h="100%"
+        mt="0px"
       >
         <Heading textAlign="center" fontSize="2xl">
           My world today
         </Heading>
-        <Text textAlign="start" fontSize="lg" px={20}>
+        <Text textAlign="start" fontSize="lg" px={{ base: 8, md: 20 }}>
           Sed ut perspiciatis unde omnis iste natus error sit voluptatem
           accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
           ab illo inventore veritatis et quasi architecto beatae vitae dicta
-          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
-          qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
+          sunt explicabo. View all days voluptatem quia voluptas sit aspernatur
+          aut odit aut fugit, sed quia consequuntur magni dolores eos qui
+          ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
           dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
           quia non numquam eius modi tempora incidunt ut labore et dolore magnam
           aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
@@ -30,7 +37,7 @@ export default function ContentPage() {
           ea voluptate velit esse quam nihil molestiae consequatur, vel illum
           qui dolorem eum fugiat quo voluptas nulla pariatur
         </Text>
-      </Stack>
+      </Flex>
     </Box>
   );
 }
