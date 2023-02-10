7aca3891d2b52d4fdc242cafd7f9a81ca5d6ba23
Lereko Foloko <84933354+Boithuto2021@users.noreply.github.com>
2023-02-10 15:28:04 +0200

    added span link in content page

--- src/components/content-page/ContentPage.tsx
+++ src/components/content-page/ContentPage.tsx
@@ -1,5 +1,5 @@
 import { Box, Heading, Flex, Text } from "@chakra-ui/react";
-import React from "react";
+import { Link } from "react-router-dom";
 
 export default function ContentPage() {
   return (
@@ -26,16 +26,25 @@ export default function ContentPage() {
           Sed ut perspiciatis unde omnis iste natus error sit voluptatem
           accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
           ab illo inventore veritatis et quasi architecto beatae vitae dicta
-          sunt explicabo. View all days voluptatem quia voluptas sit aspernatur
-          aut odit aut fugit, sed quia consequuntur magni dolores eos qui
-          ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
-          dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
-          quia non numquam eius modi tempora incidunt ut labore et dolore magnam
-          aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-          exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex
-          ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
-          ea voluptate velit esse quam nihil molestiae consequatur, vel illum
-          qui dolorem eum fugiat quo voluptas nulla pariatur
+          sunt explicabo.
+          <Text
+            as="span"
+            textDecoration="underline"
+            fontWeight="semibold"
+            p={1}
+          >
+            <Link to="#">View all days</Link>
+          </Text>
+          voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+          consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+          Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+          consectetur, adipisci velit, sed quia non numquam eius modi tempora
+          incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut
+          enim ad minima veniam, quis nostrum exercitationem ullam corporis
+          suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis
+          autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
+          nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
+          voluptas nulla pariatur
         </Text>
       </Flex>
     </Box>
